ae48672dc8b6b39a6e41b9e96ae1d2fc27e13b94
Arumino <git@arumino-om.net>
2025-05-11 13:16:44 +0900

    refactor: if→switchに変更

--- src/features/auth/AuthErrorHandle.tsx
+++ src/features/auth/AuthErrorHandle.tsx
@@ -3,18 +3,29 @@ export function AuthErrorHandle(
   enqueueSnackbar: (message: string, options: { variant: string }) => void,
 ) {
   let errorMessage = 'エラーが発生しました';
-
-  if (e.message.includes('user-not-found')) {
-    errorMessage = 'ユーザーが見つかりません';
-  } else if (e.message.includes('too-many-requests')) {
-    errorMessage =
-      '試行回数が多すぎます。しばらく時間をおいて再度お試しください';
-  } else if (e.message.includes('invalid-credential')) {
-    errorMessage = '認証情報が正しくありません';
-  } else if (e.message.includes('invalid-email')) {
-    errorMessage = 'メールアドレスのフォーマットが正しくありません';
-  } else if (e.message.includes('user-disabled')) {
-    errorMessage = 'ユーザーが無効化されています';
+  switch (e.message) {
+    case 'auth/user-not-found':
+      errorMessage = 'ユーザーが見つかりません';
+      break;
+    case 'auth/too-many-requests':
+      errorMessage =
+        '試行回数が多すぎます。しばらく時間をおいて再度お試しください';
+      break;
+    case 'auth/invalid-credential':
+      errorMessage = '認証情報が正しくありません';
+      break;
+    case 'auth/invalid-email':
+      errorMessage = 'メールアドレスのフォーマットが正しくありません';
+      break;
+    case 'auth/user-disabled':
+      errorMessage = 'ユーザーが無効化されています';
+      break;
+    case 'auth/popup-closed-by-user':
+      // 認証を取り消した場合は、インフォとして表示する
+      enqueueSnackbar('認証を取り消しました', { variant: 'info' });
+      return;
+    default:
+      console.error(e.message);
   }
 
   enqueueSnackbar(errorMessage, { variant: 'error' });
